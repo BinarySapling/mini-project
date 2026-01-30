730b47635dd2cac5ef8806cd9fb33e0406c3f410
Danish <danishanwarofficial@gmail.com>
2026-01-30 10:44:41 +0530

    add clear history button and implement functionality to clear search history

--- history.js
+++ history.js
@@ -1,17 +1,30 @@
 const historyList = document.getElementById('historyList');
 
-const history = JSON.parse(localStorage.getItem("searchHistory")) || [];
+let history = JSON.parse(localStorage.getItem("searchHistory")) || [];
 
-if(history.length > 0){
-    history.forEach(item => {
-        const div = document.createElement('div');
-        div.className = 'history-item';
-        div.innerHTML = `
-            <p>${item.query}</p>
-            <span>${new Date(item.time).toLocaleString()}</span>
-        `;
-        historyList.appendChild(div);
-    });
-} else {
-    historyList.innerHTML = '<p class="loading">No search history found</p>';
+function displayHistory() {
+    historyList.innerHTML = '';
+    if(history.length > 0){
+        history.forEach(item => {
+            const div = document.createElement('div');
+            div.className = 'history-item';
+            div.innerHTML = `
+                <p>${item.query}</p>
+                <span>${new Date(item.time).toLocaleString()}</span>
+            `;
+            historyList.appendChild(div);
+        });
+    } else {
+        historyList.innerHTML = '<p class="loading">No search history found</p>';
+    }
 }
+
+function clearHistory() {
+    if(confirm('Are you sure you want to clear all search history?')) {
+        localStorage.removeItem('searchHistory');
+        history = [];
+        displayHistory();
+    }
+}
+
+displayHistory();
