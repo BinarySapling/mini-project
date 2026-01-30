66d1409b93153e37d63e5e149ff43433fa55eb87
Danish <danishanwarofficial@gmail.com>
2026-01-30 14:32:41 +0530

    Reorganize project structure: move CSS and JS files to separate folders

---
+++ js/history.js
@@ -0,0 +1,30 @@
+const historyList = document.getElementById('historyList');
+
+let history = JSON.parse(localStorage.getItem("searchHistory")) || [];
+
+function displayHistory() {
+    historyList.innerHTML = '';
+    if(history.length > 0){
+        history.forEach(item => {
+            const div = document.createElement('div');
+            div.className = 'history-item';
+            div.innerHTML = `
+                <p>${item.query}</p>
+                <span>${new Date(item.time).toLocaleString()}</span>
+            `;
+            historyList.appendChild(div);
+        });
+    } else {
+        historyList.innerHTML = '<p class="loading">No search history found</p>';
+    }
+}
+
+function clearHistory() {
+    if(confirm('Are you sure you want to clear all search history?')) {
+        localStorage.removeItem('searchHistory');
+        history = [];
+        displayHistory();
+    }
+}
+
+displayHistory();
